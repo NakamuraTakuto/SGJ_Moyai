cef8755f3dfcbfc1e5946a14a286a5f1f92da906
narishiho0804 <56993978+narishiho0804@users.noreply.github.com>
2023-03-27 12:44:55 +0900

    [add] ResultScene・作成

---
+++ Assets/Script/Hiroto/Automatic.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Automatic : MonoBehaviour
+{
+    private float chargeTime = 5.0f;
+    private float timeCount;
+    private void Update()
+    {
+        timeCount += Time.deltaTime;
+
+        // 自動前進
+        transform.position += transform.right * Time.deltaTime;
+
+        // 指定時間の経過（条件）
+        if (timeCount > chargeTime)
+        {
+            // 進路をランダムに変更する
+            Vector3 course = new Vector3(0, Random.Range(0, 180), 0);
+            transform.localRotation = Quaternion.Euler(course);
+
+            // タイムカウントを０に戻す
+            timeCount = 0;
+        }
+    }
+}
